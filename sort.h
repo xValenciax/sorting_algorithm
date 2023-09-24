cd13e7a9c971b74430de506a3863e386935258c0
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-09-24 18:30:07 +0300

    Update

--- sort.h
+++ sort.h
@@ -44,15 +44,15 @@ void quick_sort(int *array, size_t size);
 void quick_sort_rec(int *array, int lo, int hi, size_t size);
 int lomuto_partition(int *array, int lo, int hi, size_t size);
 
-void selection_sort(int* array, size_t size);
-void shell_sort(int* array, size_t size);
-void cocktail_sort_list(listint_t** list);
-void counting_sort(int* array, size_t size);
-void merge_sort(int* array, size_t size);
-void heap_sort(int* array, size_t size);
-void radix_sort(int* array, size_t size);
-void bitonic_sort(int* array, size_t size);
-void quick_sort_hoare(int* array, size_t size);
+//void selection_sort(int* array, size_t size);
+//void shell_sort(int* array, size_t size);
+//void cocktail_sort_list(listint_t** list);
+//void counting_sort(int* array, size_t size);
+//void merge_sort(int* array, size_t size);
+//void heap_sort(int* array, size_t size);
+//void radix_sort(int* array, size_t size);
+//void bitonic_sort(int* array, size_t size);
+//void quick_sort_hoare(int* array, size_t size);
 
 
 #endif
