a0ff7a121e2d0cbc15b86148130270a480a2983a
Selim <mamo31st@gmail.com>
2023-09-24 18:53:07 +0300

    updates

--- sort.h
+++ sort.h
@@ -31,26 +31,26 @@ typedef enum bool
 typedef struct listint_s
 {
 	const int n;
-	struct listint_s* prev;
-	struct listint_s* next;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Printing helper functions */
-void print_array(const int* array, size_t size);
-void print_list(const listint_t* list);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 /* Sorting algoritms */
-void bubble_sort(int* array, size_t size);
-void insertion_sort_list(listint_t** list);
-void selection_sort(int* array, size_t size);
-void quick_sort(int* array, size_t size);
-void shell_sort(int* array, size_t size);
-void cocktail_sort_list(listint_t** list);
-void counting_sort(int* array, size_t size);
-void merge_sort(int* array, size_t size);
-void heap_sort(int* array, size_t size);
-void radix_sort(int* array, size_t size);
-void bitonic_sort(int* array, size_t size);
-void quick_sort_hoare(int* array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
